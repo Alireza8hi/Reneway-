b945731f221be1c7cb023776eff6e7683cbf1afe
Amirmehdig <amirmahdig138@gmail.com>
2025-08-04 19:48:26 +0330

    feat: Add dockerfiles and compose

--- Back-end/store/urls.py
+++ Back-end/store/urls.py
@@ -2,7 +2,7 @@ from .views import UserCreateAPIView, LoginAPIView, LogoutAPIView
 from django.urls import path
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(), name='user-register'),
-    path('login/', LoginAPIView.as_view(), name='user-login'),
-    path('logout/', LogoutAPIView.as_view(), name='user-logout'),
+    path('register', UserCreateAPIView.as_view(), name='user-register'),
+    path('login', LoginAPIView.as_view(), name='user-login'),
+    path('logout', LogoutAPIView.as_view(), name='user-logout'),
 ]
